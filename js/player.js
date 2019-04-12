f4358a009ff983003fc758817bc5d44829e88c06
Gusbe <gusbertolo@gmail.com>
2019-04-12 23:52:17 +0200

    Object player

--- js/player.js
+++ js/player.js
@@ -1,1 +1,53 @@
 'use scrict'
+
+function Player(canvas){
+  this.lifes = 3;
+  this.posX = 1;
+  this.posY = 1;
+  bombsAvailable = 1;
+  speed = 1;
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext('2d');
+}
+
+Player.prototype.move = function(x, y){
+
+  this.posX = x;
+  this.posY = y;
+}
+
+Player.prototype.NextPosition = function(direction) {
+
+  let nextPositionY = this.posY;
+  let nextPositionX = this.posX;
+
+  switch(direction){
+    case 'U': nextPositionY--;  break;  //Up
+    case 'D': nextPositionY++;  break;  //Down
+    case 'R': nextPositionX++;  break;  //Rigth
+    case 'L': nextPositionX--;  break;  //Left
+  }
+  
+  return [nextPositionX , nextPositionY];
+}
+
+Player.prototype.reduceLifes = function (){
+
+  this.lifes--;
+}
+
+Player.prototype.addBombsAvailable = function (){
+
+  this.bombsAvailable++;
+}
+
+Player.prototype.reduceBombsAvailable = function (){
+
+  this.bombsAvailable--;
+}
+
+Player.prototype.print = function (canvas) {
+
+  this.ctx.fillStyle = 'blue';
+  this.ctx.fillRect(this.posY*50, this.posX*50, 50, 50);
+}
