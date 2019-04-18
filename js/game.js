03f95df3474631ffc22f6dc1e38a01e730abce29
Gusbe <gusbertolo@gmail.com>
2019-04-18 15:48:59 +0200

    clean: game.js

--- js/game.js
+++ js/game.js
@@ -19,6 +19,7 @@ Game.prototype.movePlayer = function (direction) {
 
   let nextCoordinates = this.player.nextPosition(direction);
   let element = this.grid.getCellElement(nextCoordinates[0],nextCoordinates[1]);
+
   switch(element){
 
     case 'X':
@@ -79,6 +80,7 @@ Game.prototype.movePlayer = function (direction) {
 Game.prototype.plantBomb = function () {
 
   if(this.player.bombsAvailable > 0){
+
     this.sounds.play('plant');
     this.player.bombsAvailable--;
     this.bombs.push(new Bomb(this.canvas,this.player.posX,this.player.posY,this.player.rangeBombs));
@@ -104,6 +106,7 @@ Game.prototype.moveEnemies = function (grid) {
       this.enemies[i].GenerateRandomMovement(grid);
     
       if(this.grid.getCellElement(this.enemies[i].posX,this.enemies[i].posY) === 'F'){
+
         this.sounds.play('enemyDies');
         this.enemies.splice(i,1);
       }
@@ -112,6 +115,7 @@ Game.prototype.moveEnemies = function (grid) {
         this.dead();
       }
       else{
+
         this.grid.putInGrid('E',this.enemies[i].posX,this.enemies[i].posY);
       }
     }
@@ -142,36 +146,41 @@ Game.prototype.startLoop = function () {
 
   this.player = new Player(this.canvas);
 
+  //Creates the 5 enemies in its start position
   this.enemies.push(new Enemy(this.canvas,18,1));
   this.enemies.push(new Enemy(this.canvas,17,11));
   this.enemies.push(new Enemy(this.canvas,3,11));
   this.enemies.push(new Enemy(this.canvas,4,5));
   this.enemies.push(new Enemy(this.canvas,11,7));
 
-  
-
-
+  //LOOP
   const loop = () => {
     
+    //If the game is not over
     if(!this.gameOver){
+
       this.refreshScreen();
       this.checkIfWinner();
 
       this.updateScreenCounters(this.player.bombsAvailable);
     }
-    if (this.timeUp() <= 0) this.dead();
+
+    if (this.timeUp() <= 0) this.dead();  //If the time is over, player dies
     
+    this.moveEnemies(this.grid);  //Moves all the enemies alive
 
-    this.moveEnemies(this.grid);
-
+    //Explodes the bombs and extinguishes fires
     for(let i = 0 ; i < this.bombs.length ; i++){
 
+      //Checks the bombs and explodes the ones that has to explode
       if(this.bombs[i].checkIfExplodes()){
 
         this.explosion(this.bombs[i]);
         this.player.bombsAvailable++;
 
       }
+
+      //Checks the bombs and extinguish the ones that has to extinguish
       if(this.bombs[i].checkRemoveFire()){
         
         for(let j = 0 ; j < this.bombs[i].fireCells.length ; j++){
@@ -185,38 +194,33 @@ Game.prototype.startLoop = function () {
                 
           }
         }
-        this.bombs.splice(i, 1);
+        this.bombs.splice(i, 1);  //removes the bomb exploded from the array.
       }
     }
     
-
-
-
-    
-  
-    if(!this.gameOver && !this.winner) {
+    if(!this.gameOver && !this.winner) {  //If the game still on going
 
       window.requestAnimationFrame(loop);
     }
-    else if(this.winner){
+    else if(this.winner){ //If all the enemies are killed. Wins
 
       setTimeout(this.buildWinnerScreen,2000);
     }
-    else if(this.credits > 0){
-      this.sounds.play('dead');
+    else if(this.credits > 0){  //If the player is dead but still has lifes
 
+      this.sounds.play('dead');
       setTimeout( () => { this.buildGameOverWithLifesScreen(this.credits)} , 2000);
     }
     else if(this.credits === 0){
-      this.sounds.play('dead');
+
+      this.sounds.play('dead');  //IF the player is dead and doesn't has more lifes.
       setTimeout(this.buildGameOverScreen,2000);
     }
     
     
-    
   }
   
-    window.requestAnimationFrame(loop);
+    window.requestAnimationFrame(loop); //Calling again the loop.
     
   
 }
@@ -224,9 +228,11 @@ Game.prototype.startLoop = function () {
 Game.prototype.explosion = function (bomb) {
   
   this.sounds.play('explosion');
+
   let fireCells = bomb.getFireCells(this.grid);
   let playerDead = false;
   let arrayPowerUps = [];
+
   for(let i = 0 ; i < fireCells.length ; i++){
   
     switch (this.grid.getCellElement(fireCells[i][0],fireCells[i][1])){
@@ -236,22 +242,25 @@ Game.prototype.explosion = function (bomb) {
                 break;
 
       case 'S': //Breaks the wall & generate the powerUp
-
                 let powerUp = this.grid.roulettePowerUp();
                 
                 if(!powerUp){
+
                   this.grid.putInGrid('X', fireCells[i][0], fireCells[i][1]);
                 }
                 else{
+
                   arrayPowerUps.push([powerUp,fireCells[i][0], fireCells[i][1]]);
                 }
                 break;
 
       case 'E': //Kills the enemy
                 this.sounds.play('enemyDies');
+
                 for( let j = 0 ; j < this.enemies.length ; j++){
                 
                   if(this.enemies[j].posX === fireCells[i][0] && this.enemies[j].posY === fireCells[i][1]){
+
                     this.enemies.splice(j,1);
                     this.grid.putInGrid('X', fireCells[i][0], fireCells[i][1]);
                   }
@@ -269,6 +278,7 @@ Game.prototype.explosion = function (bomb) {
     this.grid.putInGrid('F', fireCells[i][0], fireCells[i][1]); //Put fire in the grid
 
     if(this.player.posX === fireCells[i][0] && this.player.posY === fireCells[i][1]){ //if the player is in the same cell than the bomb
+      
       playerDead = true;
     }
 
@@ -279,6 +289,7 @@ Game.prototype.explosion = function (bomb) {
   }
 
   if(playerDead){
+
     this.refreshScreen();
     this.dead('fire');
   }
@@ -301,10 +312,12 @@ Game.prototype.clearCanvas = function () {
 Game.prototype.updateCanvas = function () {
 
   this.bombs.forEach( function(bomb) {
+
     bomb.print();
   });
   
   this.enemies.forEach( function(enemy) {
+
     enemy.print();
   });
   
@@ -312,11 +325,13 @@ Game.prototype.updateCanvas = function () {
 }
 
 Game.prototype.updateScreenCounters = function (numberOfBombs){
+
   let bombsInfo = document.getElementById('bombs');
   let timeInfo = document.getElementById('time');
   
   let bombsScreen = "Bombs: ";
   for (let i = 0 ; i < numberOfBombs ; i ++){
+
     bombsScreen = bombsScreen + `<img src="./img/bomb.png" id="bombsScreen">`;
   }
   bombsInfo.innerHTML = bombsScreen;
@@ -324,10 +339,12 @@ Game.prototype.updateScreenCounters = function (numberOfBombs){
 }
 
 Game.prototype.updateScreenLifes = function (){
+
   let lifes = document.getElementById('lifes');
   
   let lifesScreen = "Lifes: ";
   for (let i = 0 ; i < this.credits ; i ++){
+    
     lifesScreen = lifesScreen + `<img src="./img/bomberman.gif" id="bombsScreen">`;
   }
   lifes.innerHTML = lifesScreen;
